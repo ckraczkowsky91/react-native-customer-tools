34587096bb2f4384d7340742f4b65df69b4691e6
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2019-09-15 21:01:53 -0700

    Initial commit

---
+++ CustomerTools/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text
+} from 'react-native';
+
+import
+
+export default class CustomerTools extends Component {
+  render(){
+    return(
+        <Text style={styles.text}>Hello world!</Text>
+    )
+  };
+};
+
+const styles = StyleSheet.create({
+  text: {
+    alignItems: 'center'
+  }
+});
